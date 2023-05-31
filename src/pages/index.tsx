5ca7a40ba058c7cd9bd11800af83a787ccf3ddf3
iqbaltz <mhdiqbaltan@gmail.com>
2023-05-31 16:13:23 +0700

    change icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,6 @@ import { Newsletter } from "@/components/common/newsletter";
 import { SocialMedia } from "@/components/common/social";
 import { Hero, About, Features, Testimonials } from "@/components/pages/home";
 import Head from "next/head";
-// import type { Metadata } from "next";
-
-// export const metadata: Metadata = {
-//   title: "Cleopatra Ogharadukun",
-//   description:
-//     "I am a business development specialist and growth advisor with over 10 years’ experience in driving growth strategies for corporate and individual clients. I have worked with some of the world’s largest consulting and advisory firms across wealth management, market research and social impact.",
-// };
 
 export default function Home() {
   return (
@@ -20,6 +13,24 @@ export default function Home() {
           name="description"
           content="I am a business development specialist and growth advisor with over 10 years’ experience in driving growth strategies for corporate and individual clients. I have worked with some of the world’s largest consulting and advisory firms across wealth management, market research and social impact."
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <Hero />
       <Newsletter />
